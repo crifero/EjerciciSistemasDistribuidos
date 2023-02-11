2e8c84ecabc58a5f114c5d76dd08afb829a025ec
crifero <crodriguezobando14@gmail.com>
2023-02-10 20:09:56 -0500

    se añade la suma

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        int a = 5;
+        int b = 6;
+        int suma = a + b;
+        System.out.println("la suma es: " + suma);
     }
 }
